09a91d159548ad541f7a3ef3a54e777d20128db0
lucaskrauss <luc.k@online.de>
2016-01-15 21:12:07 +0000

    Added skelleton for rating-quality-identification

--- src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
+++ src/main/java/edu/kit/ipd/crowdcontrol/objectservice/database/operations/AnswerRatingOperations.java
@@ -1,6 +1,13 @@
 package edu.kit.ipd.crowdcontrol.objectservice.database.operations;
 
+import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.AnswerRecord;
+import edu.kit.ipd.crowdcontrol.objectservice.database.model.tables.records.RatingRecord;
 import org.jooq.DSLContext;
+import org.jooq.Result;
+
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * responsible for all queries related to the Answer and Rating Table
@@ -11,4 +18,18 @@ public class AnswerRatingOperations extends AbstractOperation {
     protected AnswerRatingOperations(DSLContext create) {
         super(create);
     }
+
+    public  Map<AnswerRecord,List<RatingRecord>> getAnswersWithRatings(int experimentID){
+        return null;
+    }
+
+    public Result<RatingRecord> getRatingsOfAnswer(AnswerRecord answerRecord){
+        return null;
+    }
+
+    public Result<AnswerRecord> getAnswersOfExperiment(int expID){
+        return null;
+    }
+
+    public void setQualityToRatings(Map<RatingRecord,Integer> map){}
 }
